a225fd49bd64ad96697bb4b748bbb86ed358e9f4
Doug Stephen <dstephen@oxefit.com>
2020-11-10 14:11:33 -0600

    Set up a simple filter test that needs to be passed

--- test_filter.py
+++ test_filter.py
@@ -1,4 +1,5 @@
 from filter import *
+import random
 
 
 def test_filter_from_break_freq():
@@ -7,3 +8,22 @@ def test_filter_from_break_freq():
 
     filter = AlphaFilter.from_break_freq(math.inf, 0.001)
     assert filter.alpha == 0.0
+
+
+def test_filter_simple():
+    filter = AlphaFilter(0.8, 0.001)
+    assert filter.alpha == 0.8
+    assert filter.sample_dt == 0.001
+
+    filter.initialize(initial_position=10)
+    simulated_noise = 0
+
+    for i in range(10000):
+        if i % 2 == 0:
+            simulated_noise = random.random()
+
+        current_value = filter.value()
+        new_value = current_value + (math.pow(-1, i) * simulated_noise)
+        filter.do_filter(new_value)
+
+    assert math.abs(filter.value() - 10) < 0.0001
